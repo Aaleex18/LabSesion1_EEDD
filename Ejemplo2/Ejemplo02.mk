6b5e25b8d83a8baf93913d4ef496211b29768c2f
Aaleex18 <a.martinezmartin@edu.uah.es>
2024-09-19 23:25:58 +0200

    2
    2

---
+++ Ejemplo2/Ejemplo02.mk
@@ -0,0 +1,109 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Debug
+ProjectName            :=Ejemplo02
+ConfigurationName      :=Debug
+WorkspaceConfiguration :=Debug
+WorkspacePath          :=C:/Users/aleja/OneDrive/CASA/Documentos/estructura_datos_s1
+ProjectPath            :=C:/Users/aleja/OneDrive/CASA/Documentos/estructura_datos_s1/Ejemplo2
+IntermediateDirectory  :=../build-$(WorkspaceConfiguration)/Ejemplo2
+OutDir                 :=$(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=Alex
+Date                   :=19/09/2024
+CodeLitePath           :="C:/Program Files/CodeLite"
+MakeDirCommand         :=mkdir
+LinkerName             :=C:/Users/aleja/Downloads/codelite-amd64-17.0.0/codelite-amd64-17.0.0/codelite/mingw64/bin/g++.exe
+SharedObjectLinkerName :=C:/Users/aleja/Downloads/codelite-amd64-17.0.0/codelite-amd64-17.0.0/codelite/mingw64/bin/g++.exe -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputDirectory        :=C:/Users/aleja/OneDrive/CASA/Documentos/estructura_datos_s1/build-$(WorkspaceConfiguration)/bin
+OutputFile             :=..\build-$(WorkspaceConfiguration)\bin\$(ProjectName).exe
+Preprocessors          :=
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E
+ObjectsFileList        :=$(IntermediateDirectory)/ObjectsList.txt
+PCHCompileFlags        :=
+RcCmpOptions           := 
+RcCompilerName         :=C:/Users/aleja/Downloads/codelite-amd64-17.0.0/codelite-amd64-17.0.0/codelite/mingw64/bin/windres.exe
+LinkOptions            :=  
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := 
+ArLibs                 :=  
+LibPath                := $(LibraryPathSwitch). 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overridden using an environment variable
+##
+AR       := C:/Users/aleja/Downloads/codelite-amd64-17.0.0/codelite-amd64-17.0.0/codelite/mingw64/bin/ar.exe -r
+CXX      := C:/Users/aleja/Downloads/codelite-amd64-17.0.0/codelite-amd64-17.0.0/codelite/mingw64/bin/g++.exe
+CC       := C:/Users/aleja/Downloads/codelite-amd64-17.0.0/codelite-amd64-17.0.0/codelite/mingw64/bin/gcc.exe
+CXXFLAGS :=  -gdwarf-2 -O0 -Wall $(Preprocessors)
+CFLAGS   :=  -gdwarf-2 -O0 -Wall $(Preprocessors)
+ASFLAGS  := 
+AS       := C:/Users/aleja/Downloads/codelite-amd64-17.0.0/codelite-amd64-17.0.0/codelite/mingw64/bin/as.exe
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=C:\Users\aleja\Downloads\codelite-amd64-17.0.0\codelite-amd64-17.0.0\codelite
+Objects0=$(IntermediateDirectory)/main.c$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: MakeIntermediateDirs $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@if not exist "$(IntermediateDirectory)" $(MakeDirCommand) "$(IntermediateDirectory)"
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@if not exist "$(IntermediateDirectory)" $(MakeDirCommand) "$(IntermediateDirectory)"
+	@if not exist "$(OutputDirectory)" $(MakeDirCommand) "$(OutputDirectory)"
+
+$(IntermediateDirectory)/.d:
+	@if not exist "$(IntermediateDirectory)" $(MakeDirCommand) "$(IntermediateDirectory)"
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/main.c$(ObjectSuffix): main.c 
+	$(CC) $(SourceSwitch) "C:/Users/aleja/OneDrive/CASA/Documentos/estructura_datos_s1/Ejemplo2/main.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.c$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/main.c$(PreprocessSuffix): main.c
+	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.c$(PreprocessSuffix) main.c
+
+##
+## Clean
+##
+clean:
+	$(RM) -r $(IntermediateDirectory)
+
+
